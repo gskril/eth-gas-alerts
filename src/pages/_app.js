0fa45e5d3951a9f5038bd1771ffd95b52410accf
Greg <gskril@gmail.com>
2022-04-10 15:15:44 -0400

    Fix Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,34 +1,68 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
 import Head from 'next/head'
+import * as gtag from '../lib/gtag'
 
-function MyApp({ Component, pageProps }) {
-  return (
+function App({ Component, pageProps }) {
+	const router = useRouter()
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
+
+	return (
 		<>
-      <Head>
-        <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
-      </Head>
-			<Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
+			<Head>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="64x64"
+					href="/favicon-64.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16.png"
+				/>
+			</Head>
 
-      <Script strategy="lazyOnload" id="">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
 
 			<Component {...pageProps} />
 		</>
-  )
+	)
 }
 
-export default MyApp
+export default App
