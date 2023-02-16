53c90c33768a6f217c61ee372f411302120080bb
Greg <gskril@gmail.com>
2023-02-15 20:51:05 -0500

    Prettier

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,18 @@ const fs = require('fs')
 
 // Set the initial date according to MINS_BEFORE_STARTING
 const intialDate = () => {
-	const minsBeforeStarting = process.env.MINS_BEFORE_STARTING
-		? process.env.MINS_BEFORE_STARTING
-		: 0
+  const minsBeforeStarting = process.env.MINS_BEFORE_STARTING
+    ? process.env.MINS_BEFORE_STARTING
+    : 0
 
-	console.log(
-		`Waiting ${minsBeforeStarting} mins before tweeting with a target of ${process.env.TARGET_GAS_PRICE} gwei`
-	)
+  console.log(
+    `Waiting ${minsBeforeStarting} mins before tweeting with a target of ${process.env.TARGET_GAS_PRICE} gwei`
+  )
 
-	const minutes = process.env.MINS_BETWEEN_TWEETS - minsBeforeStarting
-	const now = new Date()
-	now.setMinutes(now.getMinutes() - minutes)
-	return now
+  const minutes = process.env.MINS_BETWEEN_TWEETS - minsBeforeStarting
+  const now = new Date()
+  now.setMinutes(now.getMinutes() - minutes)
+  return now
 }
 
 // Set last tweeted as start time
@@ -25,44 +25,49 @@ fs.writeFileSync('./lastTweeted.json', JSON.stringify(intialDate()))
 
 // Run job every 30 seconds
 cron.schedule('*/30 * * * * *', async () => {
-	const gas = await axios
-		.get('https://gas.best/stats')
-		.then(async (res) => {
-			const data = await res.data
-			return {
-				live: data.pending.fee,
-				hour: data.forecast['1 hour'],
-			}
-		})
-		.catch((err) => {
-			console.log('Error fetching gas.', err)
-			return null
-		})
+  const gas = await axios
+    .get('https://gas.best/stats')
+    .then(async (res) => {
+      if (!res.ok) {
+        console.log('Error fetching gas.', res)
+        return null
+      }
 
-	// Stop on error
-	if (gas === null) return
+      const data = await res.data
+      return {
+        live: data.pending.fee,
+        hour: data.forecast['1 hour'],
+      }
+    })
+    .catch((err) => {
+      console.log('Error fetching gas.', err)
+      return null
+    })
 
-	twitter.updateLocation(gas)
+  // Stop on error
+  if (gas === null) return
 
-	const now = new Date()
-	const lastTweeted = new Date(
-		JSON.parse(fs.readFileSync('./lastTweeted.json'))
-	)
-	const minsSinceLastTweet = Math.floor((now - lastTweeted) / 1000 / 60)
-	const minsToWait = process.env.MINS_BETWEEN_TWEETS - minsSinceLastTweet
+  twitter.updateLocation(gas)
 
-	if (minsToWait <= 0 && gas.live <= process.env.TARGET_GAS_PRICE) {
-		const timeStr = now.toISOString().slice(11, -5) + ' UTC'
+  const now = new Date()
+  const lastTweeted = new Date(
+    JSON.parse(fs.readFileSync('./lastTweeted.json'))
+  )
+  const minsSinceLastTweet = Math.floor((now - lastTweeted) / 1000 / 60)
+  const minsToWait = process.env.MINS_BETWEEN_TWEETS - minsSinceLastTweet
 
-		const tweet = `⛽️ Gas on #Ethereum is currently ${
-			gas.live
-		} gwei (as of ${timeStr}). ${
-			gas.hour <= gas.live - 3
-				? `\n\n⚡️ Expected to drop to ${gas.hour} gwei within the hour`
-				: 'Great time to make transactions!'
-		}`
+  if (minsToWait <= 0 && gas.live <= process.env.TARGET_GAS_PRICE) {
+    const timeStr = now.toISOString().slice(11, -5) + ' UTC'
 
-		twitter.tweet(tweet, gas.live)
-		fs.writeFileSync('./lastTweeted.json', JSON.stringify(now))
-	}
+    const tweet = `⛽️ Gas on #Ethereum is currently ${
+      gas.live
+    } gwei (as of ${timeStr}). ${
+      gas.hour <= gas.live - 3
+        ? `\n\n⚡️ Expected to drop to ${gas.hour} gwei within the hour`
+        : 'Great time to make transactions!'
+    }`
+
+    twitter.tweet(tweet, gas.live)
+    fs.writeFileSync('./lastTweeted.json', JSON.stringify(now))
+  }
 })
--- backend/twitter.js
+++ backend/twitter.js
@@ -1,39 +1,39 @@
 const Twit = require('twit')
 
 const T = new Twit({
-	consumer_key: process.env['CONSUMER_KEY'],
-	consumer_secret: process.env['CONSUMER_SECRET'],
-	access_token: process.env['ACCESS_TOKEN'],
-	access_token_secret: process.env['ACCESS_TOKEN_SECRET'],
+  consumer_key: process.env['CONSUMER_KEY'],
+  consumer_secret: process.env['CONSUMER_SECRET'],
+  access_token: process.env['ACCESS_TOKEN'],
+  access_token_secret: process.env['ACCESS_TOKEN_SECRET'],
 })
 
 // Verify valid credentials
 T.get('account/verify_credentials')
-	.then((res) => {
-		console.log('Successfully logged into', res.data.screen_name)
-	})
-	.catch((err) => {
-		console.log(err.allErrors[0].message, 'shutting down')
-		process.exit(0)
-	})
+  .then((res) => {
+    console.log('Successfully logged into', res.data.screen_name)
+  })
+  .catch((err) => {
+    console.log(err.allErrors[0].message, 'shutting down')
+    process.exit(0)
+  })
 
 // Update location
 function updateLocation(gas) {
-	T.post('account/update_profile', {
-		location: `⛽ ${gas.live} gwei  ⏰ Est. ${gas.hour} gwei in the hour`,
-		name: `ETH Gas Alerts (${gas.live} gwei)`,
-	}).catch((err) => console.log('Error updating profile', err))
+  T.post('account/update_profile', {
+    location: `⛽ ${gas.live} gwei  ⏰ Est. ${gas.hour} gwei in the hour`,
+    name: `ETH Gas Alerts (${gas.live} gwei)`,
+  }).catch((err) => console.log('Error updating profile', err))
 }
 
 // Tweet
 function tweet(msg, gas) {
-	T.post('statuses/update', { status: msg })
-		.then(
-			console.log(
-				`Tweet sent at ${gas} gwei. Waiting ${process.env.MINS_BETWEEN_TWEETS} mins before trying again`
-			)
-		)
-		.catch((err) => console.log('Error sending tweet', err))
+  T.post('statuses/update', { status: msg })
+    .then(
+      console.log(
+        `Tweet sent at ${gas} gwei. Waiting ${process.env.MINS_BETWEEN_TWEETS} mins before trying again`
+      )
+    )
+    .catch((err) => console.log('Error sending tweet', err))
 }
 
 module.exports = { updateLocation, tweet }
